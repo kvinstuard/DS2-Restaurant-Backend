0911a85706b34eb35e991b42c31b50e5c1576fa6
user <email@example.com>
2023-05-16 11:34:30 -0500

    conexion separada

--- src/controller/orders.js
+++ src/controller/orders.js
@@ -1,15 +1,7 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-     host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-      ssl: true,
-});
+const {getClient} = require('../config/database')
 
 const traerOrdenes = async (req, res) => {
+    const pool = await getClient();
     const response = await pool.query('SELECT * FROM ordenes ORDER BY id_orden ASC');
     res.status(200).json(response.rows);
 };
--- src/controller/products.js
+++ src/controller/products.js
@@ -1,22 +1,16 @@
-const { Pool } = require('pg');
+const {getClient} = require('../config/database')
 
-const pool = new Pool({
-     host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-      ssl: true,
-});
-
-const traerProductos = async (req, res) => {
+const traerProductos = async (req,res) => {
+    const pool = await getClient();
     const response = await pool.query('SELECT * FROM productos ORDER BY id_producto ASC');
     res.status(200).json(response.rows);
-}
+    
+};
+
 
 const registrarProducto = async (req, res) => {
     const { name, email } = req.body;
-    const response = await pool.query('INSERT INTO usuarios (name, email) VALUES ($1, $2)', [name, email]);
+    const response  = await pool.query('INSERT INTO usuarios (name, email) VALUES ($1, $2)', [name, email]);
     res.json({
         message: 'User Added successfully',
         body: {
